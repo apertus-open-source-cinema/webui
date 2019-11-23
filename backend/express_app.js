08b0db57eb863e312403937c8583aaad3b62d411
Jaro Habiger <jarohabiger@googlemail.com>
2019-11-24 00:35:40 +0100

    fixup of dev_deps split

--- backend/express_app.js
+++ backend/express_app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(`${__dirname}/../dist/`));
 app.use((req, res) => res.sendFile(path.resolve(`${__dirname}/../dist/index.html`)));
 
