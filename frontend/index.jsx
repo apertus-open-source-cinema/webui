d33c4750dae5e9b460ae4b3fcc6defb1843dc9c0
Jaro Habiger <jarohabiger@googlemail.com>
2019-12-21 03:06:56 +0100

    change router to also work in github pages

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core';
 import { amber, green } from '@material-ui/core/colors';
 import { ThemeProvider } from '@material-ui/core/styles';
