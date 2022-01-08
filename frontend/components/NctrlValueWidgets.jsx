be953b09ee69927921cdb9e43403953175fe8c35
Andrej <b.andrej@cumir.de>
2022-01-08 02:00:33 +0100

    Added integer option to slider control, added slope editor widget

--- frontend/components/NctrlValueWidgets.jsx
+++ frontend/components/NctrlValueWidgets.jsx
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: © 2019 Jaro Habiger <jarohabiger@googlemail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { default as React, useEffect, useState } from 'react';
-import { Button, makeStyles, MenuItem, TextField, Typography } from '@material-ui/core';
+import { Component, default as React, useEffect, useState } from 'react';
+import { Button, Container, makeStyles, MenuItem, TextField, Typography } from '@material-ui/core';
 import { usePromiseGenerator, usePromiseGeneratorRefreshable } from '../util/usePromiseGenerator';
 import Slider from '@material-ui/core/Slider';
 import ArrowLeft from '@material-ui/icons/ArrowBack';
@@ -21,6 +21,14 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     flexGrow: 1,
+  },
+  // Prevent button label from grabbing the mouse event and breaking active state
+  button_label: {
+    pointerEvents: 'none'
+  },
+  active: {
+    background: theme.palette.primary.main,
+    color: "white"
   },
   changed: {
     '& fieldset': {
@@ -111,13 +119,15 @@ export function NctrlValueTextfield({ path, rerender, rerenderDep }) {
   );
 }
 
-export function NctrlValueSlider({ path, options, min, max, rerender, rerenderDep }) {
+export function NctrlValueSlider({ path, options, min, max, integer, rerender, rerenderDep }) {
   const classes = useStyles();
+
+  const parseValue = integer ? parseInt : parseFloat
 
   const nctrlValue = NctrlValue.of(path);
   const [value, setValue] = useState(0.0);
   useEffect(() => {
-    nctrlValue.value().then(x => setValue(parseFloat(x)));
+    nctrlValue.value().then(x => setValue(parseValue(x)));
   }, [path, rerenderDep]);
 
   const MIN_SEND_DELAY = 50; // ms TODO: Maybe adjust this
@@ -134,7 +144,9 @@ export function NctrlValueSlider({ path, options, min, max, rerender, rerenderDe
   const onChangeCommitted = (e, value) =>
     nctrlValue
       .setValue(value)
-      .then(() => nctrlValue.value().then(value => setValue(parseFloat(value))))
+      .then(() => {
+          nctrlValue.value().then(value => setValue(parseValue(value)))
+      })
       .then(rerender());
 
   const renderLabel = () => {
@@ -143,13 +155,13 @@ export function NctrlValueSlider({ path, options, min, max, rerender, rerenderDe
     } else if (value < min) {
       return <ArrowLeft />;
     } else {
-      return value.toFixed(value > 100 ? 0 : 1);
+      return value.toFixed(integer || value > 100 ? 0 : 1);
     }
   };
 
   if (options) {
     const marks = Object.keys(options).map(key => ({
-      value: parseFloat(key),
+      value: parseValue(key),
       label: options[key],
     }));
 
@@ -193,31 +205,43 @@ export function NctrlValueSlider({ path, options, min, max, rerender, rerenderDe
           valueLabelDisplay="on"
           min={min}
           max={max}
-          step={0.001}
+          step={integer ? 1 : 0.001}
         />
       </div>
     );
   }
 }
 
-export function NctrlValueButtons({ path, buttons, rerender, rerenderDep }) {
+export function NctrlValueButtons({ path, buttons, selectedIndex, rerender, rerenderDep }) {
   const classes = useStyles();
 
   const nctrlValue = NctrlValue.of(path);
   const value = usePromiseGenerator(() => nctrlValue.value(), [nctrlValue, rerenderDep], '');
+  // Set first button as active by default
+  const [active, setActive] = useState(Object.keys(buttons)[0]);
 
   // the buttons are a map of label -> (formerValue) => nextValue
   return (
     <ButtonGroup size="large" className={classes.input}>
-      {Object.keys(buttons).map(label => {
+      {Object.keys(buttons).map((label, index) => {
         const clickFn = eval(buttons[label]);
+        const activeClass = active === label ? classes.active : ''
+              
         return (
           <Button
-            className={classes.button}
+            classes={{
+              label: classes.button_label
+            }}
+            className={classes.button + ' ' + activeClass}
             key={label}
-            onClick={() => nctrlValue.setValue(clickFn(value)).then(rerender)}
+            id={label}
+            onClick={(event) => {
+              setActive(event.target.id)
+              selectedIndex(index)
+              nctrlValue.setValue(clickFn(value)).then(rerender);
+            }}
           >
-            {label}
+          {label}
           </Button>
         );
       })}
@@ -232,4 +256,89 @@ export function NctrlValueSpacing({ px }) {
 export function NctrlValueText({ text }) {
   const classes = useStyles();
   return <Typography className={classes.text}>{text}</Typography>;
+}
+
+export function NctrlValueSlopeeditor({ text, hidden, rerender, rerenderDeps }) {
+  const classes = useStyles();
+  const hide = hidden ? "none" : ""
+  
+  const [selectedIndex, setSelectedIndex] = useState('');
+
+  const getSelectedIndex = (index) => {
+    setSelectedIndex(index)
+  }
+
+  const showSlopes2 = selectedIndex >= 1 ? '' : 'none';
+  const showSlopes3 = selectedIndex == 2 ? '' : 'none';
+
+  return (
+    <div style={{ display: hide }}>
+      {NctrlValueButtons({
+        path: "devices/cmv12000/cooked/number_slopes",
+          selectedIndex: getSelectedIndex,
+          buttons: {
+            "1 Slope": "x => 1",
+            "2 Slopes": "x => 2",
+            "3 Slopes": "x => 3"
+          }})}
+      <div>
+        {NctrlValueSlider({
+          path: "devices/cmv12000/computed/exposure_time_kp1_ms",
+          options:
+          {
+            64: "off",
+            70: "10 %",
+            76: "20 %",
+            83: "30 %",
+            89: "40 %",
+            95: "50 %",
+            102: "60 %",
+            108: "70 %",
+            114: "80 %",
+            121: "90 %",
+            127: "100 %",
+          }, integer: true, rerender, rerenderDeps })}
+        {NctrlValueText({ text: "Exposure Time Kneepoint 1" })}
+        {NctrlValueSlider({ path: "devices/cmv12000/computed/exposure_time_kp1_ms", min: 0, max: 15, integer: true, rerender, rerenderDeps })}
+        {NctrlValueText({ text: "Kneepoint 1 Level" })}
+      </div>
+      <div style={{ display: showSlopes2 }}>
+        {NctrlValueSlider({ path: "devices/cmv12000/computed/exposure_time_kp2_ms", options:
+          {
+            64: "off",
+            70: "10 %",
+            76: "20 %",
+            83: "30 %",
+            89: "40 %",
+            95: "50 %",
+            102: "60 %",
+            108: "70 %",
+            114: "80 %",
+            121: "90 %",
+            127: "100 %",
+          }, integer: true, rerender, rerenderDeps })}
+        {NctrlValueText({ text: "Exposure Time Kneepoint 2" })}
+        {NctrlValueSlider({ path: "devices/cmv12000/computed/exposure_time_kp2_ms", min: 0, max: 15, integer: true, rerender, rerenderDeps })}  
+        {NctrlValueText({ text: "Kneepoint 2 Level" })}
+      </div>
+      <div style={{ display: showSlopes3 }}>
+        {NctrlValueSlider({ path: "devices/cmv12000/computed/exposure_time_kp3_ms", options:
+          {
+            64: "off",
+            70: "10 %",
+            76: "20 %",
+            83: "30 %",
+            89: "40 %",
+            95: "50 %",
+            102: "60 %",
+            108: "70 %",
+            114: "80 %",
+            121: "90 %",
+            127: "100 %",
+          }, integer: true, rerender, rerenderDeps })}
+        {NctrlValueText({ text: "Exposure Time Kneepoint 3" })}
+        {NctrlValueSlider({ path: "devices/cmv12000/computed/exposure_time_kp3_ms", min: 0, max: 15, integer: true, rerender, rerenderDeps })}
+        {NctrlValueText({ text: "Kneepoint 3 Level" })}
+      </div>
+    </div>)
 }
